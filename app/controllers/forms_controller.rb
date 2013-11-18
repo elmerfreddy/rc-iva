f59be42103140296671ad862f1c6a8b7e41f0049
Elmer Mendoza <defreddyelmer@gmail.com>
2013-11-18 15:09:07 -0400

    Prepare API for update & destroy forms

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,15 +1,29 @@
 class FormsController < ApplicationController
+  before_action :set_form, only: [:update, :destroy]
+
   respond_to :json
 
   def index
-    respond_with Form.where(nil)
+    respond_with Form.all
   end
 
   def create
     respond_with Form.create(form_params)
   end
 
+  def update
+    respond_with @form.update(form_params)
+  end
+
+  def destroy
+    respond_with @form.destroy
+  end
+
   private
+
+  def set_form
+    @form = Form.find(params[:id])
+  end
 
   def form_params
     params.require(:form).permit(:nit, :invoice, :authorization, :date, :amount, :code)
